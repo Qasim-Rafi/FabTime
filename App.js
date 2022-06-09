60fcbe57cd89efc7330b7e7a922cadee098b8f29
Altaf <itxaltaf789@gmail.com>
2022-06-09 13:10:01 +0500

    fixing

--- App.js
+++ App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import {LogBox, StatusBar} from 'react-native';
-import {Provider} from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import Router from './src/navigation/Router';
-import {persistor, store} from './src/redux/store';
-import FlashMessage from 'react-native-flash-message';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React from "react";
+import { LogBox, StatusBar } from "react-native";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import Router from "./src/navigation/Router";
+import { persistor, store } from "./src/redux/store";
+import FlashMessage from "react-native-flash-message";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 const App = () => {
   return (
     <SafeAreaProvider>
       <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor} >
-         {/* <StatusBar hidden />  */}
-    <StatusBar barStyle="light-content" backgroundColor="transparent" translucent={true}/>
-
-        {/*<PersistGate loading={null} persistor={persistor}>*/}
-        <Router />
-        {/*</PersistGate>*/}
+        <PersistGate loading={null} persistor={persistor}>
+          {/* <StatusBar hidden />  */}
+          <StatusBar
+            barStyle="light-content"
+            backgroundColor="transparent"
+            translucent={true}
+          />
+          <Router />
         </PersistGate>
-        <FlashMessage position="top" icon="auto"/>
+        <FlashMessage position="top" icon="auto" />
       </Provider>
-
     </SafeAreaProvider>
   );
 };
@@ -30,8 +30,8 @@ export default App;
 console.disableYellowBox = true;
 
 LogBox.ignoreLogs([
-  'Warning: VirtualizedList should',
-  'Warning: Each child',
-  'Warning: Each child in a',
-  'Warning: Failed prop type',
+  "Warning: VirtualizedList should",
+  "Warning: Each child",
+  "Warning: Each child in a",
+  "Warning: Failed prop type",
 ]);
