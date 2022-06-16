487627a5dd64d492bffdea8f0683b3c2bcbadb26
Altaf <itxaltaf789@gmail.com>
2022-06-16 11:56:15 +0500

    Update PresentTeam.js

--- src/screens/Home/PresentTeam.js
+++ src/screens/Home/PresentTeam.js
@@ -45,7 +45,7 @@ const PresentTeam = ({route}) => {
       </View>
       <ScrollView>
 
-      {data.length > 0 ? data.map((item,index) =><CardView />) : null}
+      {data?.length > 0 ? data.map((item,index) =><CardView />) : null}
       </ScrollView>
 
     </Layout>
